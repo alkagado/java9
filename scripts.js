0a7e617d9aa57e15e194539ef07378c6d3255bcb
alkagado <38248590+alkagado@users.noreply.github.com>
2018-04-24 11:41:35 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function drawTree(treeHeight) {
+  for(var i = 1; i <= treeHeight; i++){
+    var star = "";
+    for(var j = 0; j < i; j++){
+      star += "*";
+    }
+    console.log(star);
+  }  
+}
+
+drawTree(5);
